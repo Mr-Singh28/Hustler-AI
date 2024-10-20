9223f9b5b71ac6ed572d4b5090f7f27e2fdf0507
Aurobind Singh <aurobindsingh.28@gmail.com>
2024-10-21 00:18:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-# run.py
-from src.web.app import app, create_tables
+from src.web.app import app
 
 if __name__ == '__main__':
-    create_tables()  # Call the function to create tables
-    app.run(debug=True)
+    app.run()
